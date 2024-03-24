7cf7f7ae8a28a92feb7d12688a08ce197f025716
Bill Borkowski <b1llstar@mac-mini.mynetworksettings.com>
2024-03-24 17:06:43 -0400

    fcm support, with images

---
+++ ios/Childfree Connection/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  Childfree Connection
+//
+//  Created by Bill Borkowski on 3/24/24.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
